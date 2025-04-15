f1d1334c4901881c7aab185a0661d04c285afd8d
hwcctn <2738268497@qq.com>
2025-04-15 21:32:59 +0800

    环境变量配置

--- src/service/config/index.ts
+++ src/service/config/index.ts
@@ -1,7 +1,8 @@
 // 1.手动切换
-export const BASE_URL = 'http://110.41.185.84:8888'
-// export const BASE_URL = 'https://www.wanandroid.com'
+export const BASE_URL = import.meta.env.VITE_API_URL
 export const TIME_OUT = 10000
+console.log('API URL:', BASE_URL)
+// export const BASE_URL = 'https://www.wanandroid.com'
 
 // 2.依赖当前环境: development/production
 // console.log(process.env.NODE_ENV)
@@ -15,9 +16,10 @@ export const TIME_OUT = 10000
 // export { BASE_URL }
 
 // 3.从定义的环境变量的配置文件中, 加载变量
-// console.log(process.env.REACT_APP_BASE_URL)
-// 在.env.development 中集成环境变量，注意自定义变量以REACT_APP开头
+// console.log(process.env.VUE_APP_BASE_URL)
+// 在.env.development 中集成环境变量，注意自定义变量以VUE_APP开头
 // 在.env.production
 // 在 .env.test
 // 还要在shims-vue.d.ts中声明
 // declare const VUE_APP_BASE_URL:string
+// Vite 使用 .env 文件，但变量名必须以 VITE_ 开头（而不是 VUE_APP_）。
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import AutoImport from 'unplugin-auto-import/vite'
@@ -6,41 +6,44 @@ import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 
-import { BASE_URL } from './src/service/config/'
+// import { BASE_URL } from './src/service/config/'
 import path from 'path'
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    createSvgIconsPlugin({
-      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
-      symbolId: 'icon-[dir]-[name]'
-    }),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-      dts: 'src/auto-imports.d.ts'
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()]
-    })
-  ],
-  base: './',
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src')
-    }
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: BASE_URL,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-        // bypass(req, res, options) {
-        //   const finalPath = options.rewrite!(req.url || '') || ''
-        //   const targetUrl = `${options.target}${finalPath}` // 直接拼接字符串
-        //   console.log('代理目标路径:', targetUrl)
-        // }
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  return {
+    plugins: [
+      vue(),
+      createSvgIconsPlugin({
+        iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+        symbolId: 'icon-[dir]-[name]'
+      }),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+        dts: 'src/auto-imports.d.ts'
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()]
+      })
+    ],
+    base: './',
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src')
+      }
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+          // bypass(req, res, options) {
+          //   const finalPath = options.rewrite!(req.url || '') || ''
+          //   const targetUrl = `${options.target}${finalPath}` // 直接拼接字符串
+          //   console.log('代理目标路径:', targetUrl)
+          // }
+        }
       }
     }
   }
